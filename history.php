217ec165c5d7217a4728b52b35bf09f186c399b6
Wakdanno Daniel <wakdannodan52@gmail.com>
2020-01-17 23:55:27 -0800

    third commit

---
+++ history.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Welcome EOBT</title>
+<link rel="stylesheet" type="text/css" href="xres/css/style.css" />
+<link rel="icon" type="image/png" href="xres/images/favicon.png" />
+<link type="text/css" href="css/styles.css" rel="stylesheet" media="all" />
+</head>
+
+<body>
+<div id="wrapper">
+	<div id="header">
+    
+        <ul id="mainnav">
+			<li><a href="index.php">Home</a></li>
+            <li><a href="gallery.php">Gallery</a></li>
+            <li class="current"><a href="history.php">History</a></li>
+            <li><a href="routes.php">Routes</a></li>
+            <li><a href="location.php">location</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+    	</ul>
+	</div>
+    <div id="content">
+    	<div id="gallerycontainer">
+			<div class="portfolio-area" style="margin:0 auto; padding:140px 20px 20px 20px; width:820px;">	
+					Welcome to "Ethiopian Online Bus Ticketing System"..... a newly opened cross country online bus ticket reservation system located on Adama City. What makes us different? Well, our system is not just booking. EOBT offers a newly updated news and accomodations like hotel, super market, guest houses, such things  on the advertisment site on our website.
+					<br>
+					<br>
+					We offer a special menu while you are traveling, like dry cake and soft cakes also, breads and soft drinks, ..
+					
+					<br>
+					<br>
+					Oue drivers are very outstanding and also have a good driving skills minimum of 10 years of driving skill.
+					<br>
+					<br>
+					Our buses are the best and comfortable buses in the country, they are very comfortable with newly updated technology and with a an excllent speed. 
+					<br>
+					<br>
+					At EOBT, we AIM to give you TOTAL SATISFACTION! What are you guys waiting for? Spread the word and let us pamper you with what we've got!
+					<br>
+					<br>
+					We are open daily from 2am to 12pm. SEE YOU THERE!
+					<br>
+				<div class="column-clear"></div>
+            </div>
+			<div class="clearfix"></div>
+        </div>
+    </div>
+    
+
+
+
+<div id="footer">
+	<h4>+251 933881834 &bull; <a href="contact-us.php">Ethiopia, Oromia, Adama City  </a></h4>
+	<p>Hours of Operation&nbsp;&nbsp;&bull;&nbsp;&nbsp;Mon - Sun: 2:00 am - 12:00 pm</p>
+	<p>&copy; Copyright 2012 EOBT | All Rights Reserved <br /></p>
+</div>
+
+</div>
+</body>
+</html>
